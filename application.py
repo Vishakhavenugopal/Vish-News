a8717c93944128b5f5387cc63e3d44e76a287899
Vishakhavenugopal <vishakhav965@gmail.com>
2021-10-28 17:02:36 +0530

    initial commit

---
+++ application.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+import requests
+from werkzeug.wrappers import request
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=b31c02048df346b0bd193d11c1d03762"
+    req = requests.get(url).json()
+    cases={
+        'articles': req['articles'],
+        
+    }
+    return render_template("index.html",case=cases)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
